e6a801c47d62a4678393b3ce18217240fa813ce0
Irmiya <147790437+Irmiya07@users.noreply.github.com>
2026-03-08 23:30:31 +0530

    Update CORS middleware to allow deployed frontend

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",      // local dev
+      "https://irmiya.netlify.app"  // deployed frontend 
+    ],
     credentials: true
 }));
 app.use(express.json());
